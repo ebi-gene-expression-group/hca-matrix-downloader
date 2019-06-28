eb33d4cffe167da9ae45f35fd8f6762b32f55ccd
nh3 <nh3@users.noreply.github.com>
2019-06-28 11:54:18 +0100

    update setup.py to include hca-mtx-to-10x and set entry point for client.py

--- setup.py
+++ setup.py
@@ -10,17 +10,22 @@ with open('requirements.txt', 'r') as fh:
     requirements = fh.readlines()
 
 setup(
-    name='hca_dcp_client',
+    name='hca_matrix_downloader',
     version=version,
     author='ktpolanski',
     author_email='ktpolanski@users.noreply.github.com',
-    description='Python client for the DCP matrix service',
+    description='Python client for the HCA DCP matrix service',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/ebi-gene-expression-group/dcp-matrix-service-client',
+    url='https://github.com/ebi-gene-expression-group/hca-matrix-downloader',
     packages=find_packages(),
     scripts=[
-        'hca_matrix_service_client.py',
+        'hca-mtx-to-10x',
     ],
+    entry_points=dict(
+        console_scripts=[
+            'hca-matrix-downloader=hca_matrix_service_client:main',
+        ]
+    ),
     install_requires=requirements,
 )
