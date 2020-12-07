2fb6ba7c91098721aef594d43275355fbdb21ec1
ESapenaVentura <sapenaventura@gmail.com>
2020-12-07 12:27:39 +0000

    Changed project index loading to load from FTP

--- hca_matrix_service/client.py
+++ hca_matrix_service/client.py
@@ -68,9 +68,7 @@ def download_file(project_uuid, file_format, prefix, project_info):
 def main():
     # parse the command line arguments
     args = parse_args()
-    # loaded_index = load_project_index(path)
-    with open("hca_dcp_project_index.json", "r") as pi:
-        loaded_index = json.load(pi)
+    loaded_index = load_project_index('ftp://ftp.ebi.ac.uk/pub/databases/hca-dcp/hca_dcp_project_index.json')
     requested_project_uuid = get_project_uuid(args.project, loaded_index)
     download_file(requested_project_uuid, args.format, args.outprefix, loaded_index[requested_project_uuid])
     print("Project matrix successfully downloaded.")
